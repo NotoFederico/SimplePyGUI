2aec8223db0576e757c9e613774f313482089418
Federico Noto <antoniofnoto@gmail.com>
2021-04-19 20:13:57 -0300

    Update

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class Ventana():
     __builder = None
     __lblLabel = None
     __wndMain = None
-
+s
     def __init__(self, gladefile):
         builder = Gtk.Builder()
         builder.add_from_file(gladefile)
@@ -21,12 +21,12 @@ class Ventana():
         self.__window.show_all()
 
     def on_helpButton_clicked(self, button):
-        self.__label.set_text("Presione el boton 'OK' para dirigirse al sitio web")
-        print("Hecho por Federico Noto")
+        self.__label.set_text("Press OK to go to Mars Site")
+        print("Made by Federico Noto")
 
     def on_okButton_clicked(self, button):
         webbrowser.open("https://mars.nasa.gov/mars2020", new=1)
-        print("Usted esta siendo redirigido al sitio web de la mision del Perseverance de la Nasa! ")
+        print("Going to NASA's Perseverance Site!")
 
 if __name__ == "__main__":
     ventana = Ventana("gui01.glade")
